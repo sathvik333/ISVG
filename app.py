e55cd8eb69382358dcb3de47b7ed01ad2079e9f8
unknown <nyshadhamsathvik@gmail.com>
2024-01-24 21:16:54 +0530

    Basic UI and OpenAI/CLIP model

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from crackHead import get_top_N_images
+from crackHead import plot_images_by_side
+import tempfile
+import os
+from crackHead import get_top_N_images, plot_images_by_side, image_data_df
+
+st.set_page_config(
+    layout="wide"
+)
+
+def main():
+    st.markdown("<h1 style='text-align: center; color: white;'>Search Anything App</h1>", unsafe_allow_html=True)
+
+
+    query = st.text_input("Enter your query:")
+    if st.button("Search"):
+        if len(query) > 0:
+            Query = get_top_N_images(query)
+            st.warning("Your query was "+query)
+            st.subheader("Search Results:")
+            for idx in Query.index.values:
+                image = Query.iloc[idx].jpg
+                caption = Query.iloc[idx].json
+                sim_score = Query.iloc[idx].cos_sim
+                sim_score = 100*float("{:.2f}".format(sim_score))
+                st.image(image, caption=f"Caption: {caption}\nSimilarity: {sim_score}%")
+        else:
+            st.warning("Developer is not dumb are you?")
+
+if __name__ == "__main__":
+    main()
---
+++ crackHead.py
@@ -0,0 +1,154 @@
+import os
+import torch
+import skimage
+import requests
+import numpy as np
+import pandas as pd
+from PIL import Image
+from io import BytesIO
+import IPython.display
+import matplotlib.pyplot as plt
+from datasets import load_dataset
+from collections import OrderedDict
+from transformers import CLIPProcessor, CLIPModel, CLIPTokenizer
+from io import BytesIO
+from PIL import Image
+import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import cosine_similarity
+     
+     
+image_data = load_dataset("Slapping/FINAL",split="train",)
+image_data_df = pd.DataFrame(image_data)
+
+def get_image(jpg):
+    image_bytes = BytesIO(jpg.tobytes())
+    image = Image.open(image_bytes).convert("RGB")
+    return image
+
+def get_image_caption(json):
+
+    return image_data[json]["caption"]
+
+
+
+def get_model_info(model_ID, device):
+
+  # Save the model to device
+  model = CLIPModel.from_pretrained(model_ID).to(device)
+
+  # Get the processor
+  processor = CLIPProcessor.from_pretrained(model_ID)
+
+  # Get the tokenizer
+  tokenizer = CLIPTokenizer.from_pretrained(model_ID)
+
+  # Return model, processor & tokenizer
+  return model, processor, tokenizer
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+model_ID = "openai/clip-vit-base-patch32"
+
+model, processor, tokenizer = get_model_info(model_ID, device)
+
+def get_single_text_embedding(text):
+
+  inputs = tokenizer(text, return_tensors = "pt").to(device)
+
+  text_embeddings = model.get_text_features(**inputs)
+
+  # convert the embeddings to numpy array 
+  embedding_as_np = text_embeddings.cpu().detach().numpy()
+
+  return embedding_as_np
+
+def get_all_text_embeddings(df, text_col):
+
+   df["text_embeddings"] = df[str(text_col)].apply(get_single_text_embedding)
+
+   return df
+
+image_data_df = get_all_text_embeddings(image_data_df, "json")
+
+def get_single_image_embedding(my_image):
+
+  image = processor(
+      text = None,
+      images = my_image, 
+      return_tensors="pt"
+  )["pixel_values"].to(device)
+
+  embedding = model.get_image_features(image)
+
+  # convert the embeddings to numpy array
+  embedding_as_np = embedding.cpu().detach().numpy()
+
+  return embedding_as_np
+
+def get_all_images_embedding(df, img_column):
+
+  df["img_embeddings"] = df[str(img_column)].apply(get_single_image_embedding)
+
+  return df
+
+image_data_df = get_all_images_embedding(image_data_df, "jpg")
+
+def plot_images(images):
+
+  for image in images:
+    plt.imshow(image)
+    plt.show()
+
+def plot_images_by_side(top_images):
+
+  index_values = list(top_images.index.values)
+  list_images = [top_images.iloc[idx].jpg for idx in index_values] 
+  list_captions = [top_images.iloc[idx].json for idx in index_values] 
+  similarity_score = [top_images.iloc[idx].cos_sim for idx in index_values] 
+
+  n_row = n_col = 2
+
+  _, axs = plt.subplots(n_row, n_col, figsize=(12, 12))
+  axs = axs.flatten()
+  for img, ax, caption, sim_score in zip(list_images, axs, list_captions, similarity_score):
+      ax.imshow(img)
+      sim_score = 100*float("{:.2f}".format(sim_score))
+      ax.title.set_text(f"Caption: {caption}\nSimilarity: {sim_score}%")
+  plt.show()
+
+def get_top_N_images(query, data=image_data_df, top_K=4, search_criterion="text"):
+
+    """
+    Retrieve top_K (5 is default value) articles similar to the query
+    """
+    # Text to image Search
+    if(search_criterion.lower() == "text"):
+      query_vect = get_single_text_embedding(query)
+
+    # Image to image Search
+    else: 
+      query_vect = get_single_image_embedding(query)
+
+    # Relevant columns
+    revevant_cols = ["json", "jpg", "cos_sim"]
+    
+    # Run similarity Search
+    data["cos_sim"] = data["img_embeddings"].apply(lambda x: cosine_similarity(query_vect, x))
+
+    data["cos_sim"] = data["cos_sim"].apply(lambda x: x[0][0])
+    
+    """
+    Sort Cosine Similarity Column in Descending Order 
+    Here we start at 1 to remove similarity with itself because it is always 1
+    """
+    most_similar_articles = data.sort_values(by='cos_sim', ascending=False)[1:top_K+1]
+    
+    return most_similar_articles[revevant_cols].reset_index()
+
+query_caption = "uml"
+
+top_images = get_top_N_images(query_caption)
+# print("Query: {}".format(query_caption))
+# top_images
+
+plot_images_by_side(top_images)
